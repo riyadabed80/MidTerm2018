0a9fed99862a42b44bb421cfa07d16add5b5f8e6
Anthony <anth0ny0153@gmail.com>
2018-05-10 14:25:05 -0400

    added two classes, Payment and Product

---
+++ MidTerm2018/Payment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm2018
+{
+    class Payment
+    {
+        #region data members
+        private decimal total;
+
+        #endregion
+        #region properties
+        public decimal Total
+        {
+            get
+            {
+                return total;
+            }
+            set
+            {
+                total = value;
+            }
+        }
+        #endregion
+        #region constructor
+        public Payment()
+        {
+
+        }
+        public Payment(decimal TOTAL)
+        {
+            Total = TOTAL;
+        }
+        #endregion
+        #region methods
+
+        #endregion
+    }
+}
---
+++ MidTerm2018/Product.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm2018
+{
+    class Product
+    {
+        #region data members
+        private string name;
+        private string category;
+        private string description;
+        private decimal price;
+        #endregion
+        #region properties
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+            set
+            {
+                category = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+        #endregion
+        #region constructor
+        public Product()
+        {
+
+        }
+        public Product(string NAME, string CATEGORY, string DESCRIPTION, decimal PRICE)
+        {
+            Name = NAME;
+            Category = CATEGORY;
+            Description = DESCRIPTION;
+            Price = PRICE;
+        }
+        #endregion
+        #region methods
+        // use a method to calculate the amount cost for the item by taking user input quantity 
+        #endregion
+    }
+}

cf0959edbd26960ba7b44b4f2ac08dba00e24773
Jasmine Hollis <jasmine.hollis@rocketmail.com>
2018-05-10 16:38:00 -0400

    First commit of Check Class

---
+++ MidTerm2018/Check.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm2018
+{
+    class Check : Payment
+    {
+        #region Data Members
+        private string checkNum;
+        #endregion
+
+        #region Properties
+        public string CheckNum
+        {
+            get { return checkNum; }
+            set { checkNum = value; }
+        }
+        #endregion
+
+        #region Default Constructor
+        public Check ()
+        {
+
+        }
+        #endregion
+
+        #region Method Overload
+        public Check (string check)
+        {
+            CheckNum = check;
+        }
+        #endregion
+
+
+
+    }
+}

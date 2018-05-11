d5e5b8dad62f99410db131c7ea7c825714f99c04
Jasmine Hollis <jasmine.hollis@rocketmail.com>
2018-05-11 13:43:31 -0400

    Product.txt changes, plus FileEdit.cs

---
+++ MidTerm2018/FileEdit.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+using System.IO;
+using MidTerm2018;
+
+
+
+namespace Mid_Term_Project
+
+{
+
+    class FileEdit
+
+    {
+
+        public static List<Product> Menuing()
+
+        {
+
+            //read from text file
+
+            StreamReader Reader = new StreamReader("../../Products.txt");
+
+            //empty list
+
+            List<Product> products = new List<Product>();
+
+
+
+            //save the text file in single string format
+
+            string line = Reader.ReadLine();
+
+
+
+            while (!String.IsNullOrEmpty(line))
+
+            {
+                string[] LinebyLine = line.Split('|');
+
+                products.Add(new Product(LinebyLine[0], LinebyLine[1], LinebyLine[2], double.Parse(LinebyLine[3])));
+
+                line = Reader.ReadLine();
+            }
+
+            return products;
+
+
+
+        }
+
+    }
+
+}
+

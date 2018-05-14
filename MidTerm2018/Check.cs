a753a60cc208d07b43c1c0b9f35da39ba3d63ecc
Riyad <rayabed@gmail.com>
2018-05-14 10:20:48 -0400

    Monday updates2

---
+++ MidTerm2018/Check.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace MidTerm2018
+{
+    class Check : Payment
+    {
+        #region Data Members
+        private string checkNum;
+        #endregion
+        #region Properties
+        public string CheckNum
+        {
+            get { return checkNum; }
+            set { checkNum = value; }
+        }
+        #endregion
+
+
+
+
+        #region Default Constructor
+
+        public Check()
+        {
+
+        }
+
+        public Check(double Total, string CheckNumber) : base(Total)
+        {
+            CheckNum = CheckNumber;
+        }
+        #endregion
+
+        #region Method Overload
+        public static bool Checking(string input)
+        {
+            string a = "";
+            return (Regex.IsMatch(a, @"^([0-9]{9}"));
+
+        }
+        #endregion
+        #region Method
+        #endregion
+    }
+}
